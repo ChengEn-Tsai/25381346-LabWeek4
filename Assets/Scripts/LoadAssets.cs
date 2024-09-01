eb7c9f786a0860bb35e0ed45884955f4e0384c03
Cheng-En Tsai <cheng-en.tsai@student.uts.edu.au>
2024-09-01 12:32:26 +1000

    60 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class LoadAssets : MonoBehaviour
 {
-    public int n = 0;
+    public GameObject redObj;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test");
+        //redObj.transform.position = new Vector3()
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
