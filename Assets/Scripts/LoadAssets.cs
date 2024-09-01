a3de155b9bf13846a8a7b3acbf65e5755c2f884c
Cheng-En Tsai <cheng-en.tsai@student.uts.edu.au>
2024-09-01 22:59:05 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject blueObj;
+    // Start is called before the first frame update 
     void Start()
     {
-        //redObj.transform.position = new Vector3()
+        Vector3 redPosition = new Vector3(2,0,0);
+        Quaternion redRotation = Quaternion.identity;
+        Vector3 bluePosition = new Vector3(-2, 0, 0);
+        Quaternion blueRotation = Quaternion.identity;
+        Instantiate(redObj, redPosition, redRotation);
+        Instantiate(blueObj, bluePosition, blueRotation);
+
+        Application.targetFrameRate = 60;
+
+        //redObj.transform.position = new Vector3(2, 0, 0);
+        //redObj.transform.rotation = Quaternion.identity;
+        //blueObj.transform.position = new Vector3(-2, 0, 0);
+        //blueObj.transform.rotation = Quaternion.identity;
     }
 
     // Update is called once per frame
